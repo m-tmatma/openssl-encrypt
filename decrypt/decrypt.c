53592bd49e4d2282c860e1cb0528dedff823414a
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-10-03 20:14:03 +0900

    Use Salt to create key and iv

--- decrypt/decrypt.c
+++ decrypt/decrypt.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 #include <openssl/conf.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
 
 #define BUFFER_SIZE 256
 #define KEY_SIZE 32 // for EVP_aes_256_cbc()
+
+void create_key_iv(unsigned char salt[PKCS5_SALT_LEN], unsigned char key[EVP_MAX_KEY_LENGTH], unsigned char iv[EVP_MAX_IV_LENGTH])
+{
+    const char *str = "key45678901234567890123456789012";
+    const int str_len = strlen(str);
+    const int iter = 10000;
+    const EVP_CIPHER *cipher = EVP_aes_256_cbc();
+    const int iklen = EVP_CIPHER_key_length(cipher);
+    const int ivlen = EVP_CIPHER_iv_length(cipher);
+    const EVP_MD *dgst = EVP_sha256();
+    const int islen = PKCS5_SALT_LEN;
+    unsigned char tmpkeyiv[EVP_MAX_KEY_LENGTH + EVP_MAX_IV_LENGTH];
+
+    PKCS5_PBKDF2_HMAC(str, str_len, salt, islen, iter, dgst, iklen+ivlen, tmpkeyiv);
+
+    memcpy(key, tmpkeyiv, iklen);
+    memcpy(iv, tmpkeyiv+iklen, ivlen);
+}
 
 /*!
     https://wiki.openssl.org/index.php/EVP_Symmetric_Encryption_and_Decryption
@@ -20,21 +39,60 @@ int main(int argc, char *argv[])
     unsigned char plaintext[BUFFER_SIZE+KEY_SIZE];
     unsigned char ciphertext[BUFFER_SIZE];
     int plaintext_len;
-    unsigned char *key;
-    unsigned char *iv;
+    unsigned char salt[PKCS5_SALT_LEN] = "";
+    unsigned char key[EVP_MAX_KEY_LENGTH] = "";
+    unsigned char iv[EVP_MAX_IV_LENGTH] = "";
     FILE * fpIn = NULL;
     FILE * fpOut = NULL;
     unsigned char * infile = NULL;
     unsigned char * outfile = NULL;
+    static const char magic[] = "Salted__";
+    unsigned char readmagic[sizeof(magic) - 1] = "";
+    size_t n;
 
     infile = argv[1];
     outfile = argv[2];
-    key = "iv45678901234567890123456789012";
-    iv  = "iv45678901234567890123456789012";
     outfile = argv[2];
+
+    fpIn = fopen(infile, "rb");
+    if (fpIn == NULL) {
+        printf("ERROR: fopen(%s)\n", infile);
+        return 1;
+    }
+
+    fpOut = fopen(outfile, "wb");
+    if (fpOut == NULL) {
+        fclose(fpIn);
+        printf("ERROR: fopen(%s)\n", outfile);
+        return 1;
+    }
+
+    n = fread(readmagic, 1, sizeof(readmagic), fpIn);
+    if (n != sizeof(readmagic)) {
+        fclose(fpOut);
+        fclose(fpIn);
+        printf("ERROR: fread(magic)\n");
+        return 1;
+    }
+    if (memcmp(readmagic, magic, sizeof(readmagic)) != 0){
+        fclose(fpOut);
+        fclose(fpIn);
+        printf("ERROR: readmagic\n");
+        return 1;
+    }
+    n = fread(salt, 1, sizeof(salt), fpIn);
+    if (n != sizeof(salt)) {
+        fclose(fpOut);
+        fclose(fpIn);
+        printf("ERROR: fread(salt)\n");
+        return 1;
+    }
+    create_key_iv(salt, key, iv);
 
     ctx = EVP_CIPHER_CTX_new();
     if (ctx == NULL) {
+        fclose(fpOut);
+        fclose(fpIn);
         printf("ERROR: EVP_CIPHER_CTX_new()\n");
         return 1;
     }
@@ -48,23 +106,8 @@ int main(int argc, char *argv[])
 
     printf("EVP_CIPHER_CTX_key_length() : keysize = %d\n", EVP_CIPHER_CTX_key_length(ctx));
 
-    fpIn = fopen(infile, "rb");
-    if (fpIn == NULL) {
-        EVP_CIPHER_CTX_free(ctx);
-        printf("ERROR: fopen(%s)\n", infile);
-        return 1;
-    }
-
-    fpOut = fopen(outfile, "wb");
-    if (fpOut == NULL) {
-        fclose(fpIn);
-        EVP_CIPHER_CTX_free(ctx);
-        printf("ERROR: fopen(%s)\n", outfile);
-        return 1;
-    }
-
     while(1){
-        size_t n = fread(ciphertext, 1, BUFFER_SIZE, fpIn);
+        n = fread(ciphertext, 1, BUFFER_SIZE, fpIn);
         if (n > 0 ){
             ciphertext_len = n;
 
